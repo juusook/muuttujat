651741a4af6ff8822c3917d929d5795716893898
juuso <juuso.kaukko@gmail.com>
2025-09-14 23:10:29 +0300

    muutokset

---
+++ module 7/lentokenttien hakeminen ja tallentaminen.py
@@ -0,0 +1,30 @@
+def main ():
+    airports = {} # {ICAO, name}
+    while True:
+
+        print("Airport Data Management")
+        print("1. Enter a new airport")
+        print("2. Fetch airport information")
+        print("3. Quit")
+        choice = input("Please choose an option (1-3): ")
+
+        if choice == "1":
+            icao = input("Enter the ICAO code: ").strip().upper()
+            name = input("Enter the airport name: ").strip()
+            airports[icao] = name
+            print(f"Airport {name} with ICAO code {icao} has been added.\n")
+
+        elif choice == "2":
+            icao = input("Enter the ICAO code: ").strip().upper()
+            if icao in airports:
+                print(f"The airport with ICAO code {icao} is {airports[icao]}.\n")
+            else:
+                print(f"No airport found with ICAO code {icao}.\n")
+
+        elif choice == "3":
+            print("Thank you for using the Airport Data Management system. Goodbye!")
+            break
+        else:
+            print("Unvalid option. Please choose 1, 2 or 3.")
+
+main()
