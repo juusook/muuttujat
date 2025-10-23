815c256c63390fbcaa5997d054d943940894adc8
juuso <juuso.kaukko@gmail.com>
2025-10-23 17:01:20 +0300

    Valmis

---
+++ Module 9/Car.py
@@ -0,0 +1,44 @@
+#tehtävä 1
+class Car:
+    def __init__(self, license_plate, maximum_speed=142):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    #tehtävä 2
+    def accelerate(self, accelerate):
+        self.current_speed += accelerate
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed < 0:
+            self.current_speed = 0
+        else:
+            self.current_speed = self.current_speed
+        return
+
+    #tehtävä 3
+    def drive(self, travelled_hours = 0):
+        travelled_hours += travelled_hours
+        self.travelled_distance += travelled_hours * self.current_speed
+        return
+
+car = Car("ABC-123", 142)
+print(f"Initial distance: {car.travelled_distance} km")
+car.accelerate(30)
+car.drive(1.5)
+print(f"Current speed: {car.current_speed} km/h")
+car.accelerate(30)
+car.drive(1.5)
+print(f"Current speed: {car.current_speed} km/h")
+car.accelerate(90)
+car.drive(1.5)
+print(f"Current speed: {car.current_speed} km/h")
+print(f"Distance after driving hours at 60 km/h: {car.travelled_distance} km")
+
+
+
+'''print(f"License plate: {car.license_plate} \nMaximum speed: "
+      f"{car.maximum_speed} km/h"
+      f"\nCurrent speed: {car.current_speed} km/h"
+      f" \nTravelled distance: {car.travelled_distance} km")'''
