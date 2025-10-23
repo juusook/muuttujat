072166f853a4a8a0ea13c5d041a622dd62067fb6
juuso <juuso.kaukko@gmail.com>
2025-10-23 17:38:23 +0300

    Valmis

--- Module 9/car_race.py
+++ Module 9/car_race.py
@@ -3,7 +3,7 @@ import random
 class Car:
     def __init__(self, license_plate):
         self.license_plate = license_plate
-        self.maximum_speed = 142
+        self.maximum_speed = random.randint(100, 200)
         self.current_speed = 0
         self.travelled_distance = 0
         self.travelled_hours = 0
@@ -23,20 +23,47 @@ class Car:
         self.travelled_distance += hours * self.current_speed
         return
 
-def make_car():
+def race():
     cars = []
     for i in range(1, 11):
         car = Car(f"ABC-{i}")
         cars.append(car)
 
+    race_run = True
+    while race_run:
+        for car in cars:
+            car.accelerate(random.randint(-10, 15))
+            car.drive(1)
+
+        for car in cars:
+            if car.travelled_distance > 10000:
+                race_run = False
+    return cars
+
+
+race_results = race()
+print(f"Race completed! {len(race_results)} cars participated.")
+
+for i, car in enumerate(race_results[:3]):
+    print(f"{i+1}. {car.license_plate}: {car.travelled_distance:.1f} km (speed: {car.current_speed} km/h)")
+
+winner = race_results[0]
+print(f"\nWinner: {winner.license_plate} with {winner.travelled_distance:.1f} km!")
 
 
 
 
-car = Car("ABC-123", 142)
+
+'''lista = race()
+for car in lista:
+    print(car.license_plate)
+'''
+
+
+'''car = Car("ABC-123", 142)
 print(f"Initial distance: {car.travelled_distance} km")
 car.accelerate(30)
 print(f"Current speed: {car.current_speed} km/h")
 print(f"Distance after driving hours at 60 km/h: {car.travelled_distance} km")
-
+'''
 
