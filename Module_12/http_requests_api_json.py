95fe65e7387025cb91a3981f123e0f6aa6578a4b
juuso <juuso.kaukko@gmail.com>
2025-10-28 14:35:52 +0200

    toimii

---
+++ Module_12/http_requests_api_json.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+hakusana = "xfiles"
+
+url = "https://api.tvmaze.com/search/shows?q=" + hakusana
+
+show = jso
+
+response = requests.get(url)
+
+json_data = response.json()
+
+print(json_data)
+#ensimmäinen alkio json datasta eli sanakirja
+print(json_data[0])
+print(json_data[0]['show'])
+print(json_data[0]['show']['name'])
+print(json_data[0]['show']['genres'][2])
+
+print(json.dumps(json_data, indent=2))
