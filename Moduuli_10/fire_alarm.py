d9ff723f85c2539ff6a08b37cab4cb35de9844af
juuso <juuso.kaukko@gmail.com>
2025-11-01 19:06:49 +0200

    valmiit

--- Moduuli_10/fire_alarm.py
+++ Moduuli_10/fire_alarm.py
@@ -1,8 +1,8 @@
 class Elevator:
     def __init__(self, top_floor, bottom_floor):
-        self.top = top_floor
-        self.bottom = bottom_floor
-        self.current_floor = self.bottom
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.current_floor = self.bottom_floor
 
     def floor_up(self):
         self.current_floor = self.current_floor + 1
@@ -35,9 +35,15 @@ class Building:
         elevator = self.elevators[e_num]
         elevator.go_to_floor(target_floor)
 
+    def fire_alarm(self):
+        for i in self.elevators:
+            i.go_to_floor(self.bottom_floor)
+
+
 
 # Test Building with multiple elevators
 building = Building(1, 10, 3)
 building.run_elevator(0, 5)
 building.run_elevator(1, 3)
 building.run_elevator(2, 8)
+building.fire_alarm()
