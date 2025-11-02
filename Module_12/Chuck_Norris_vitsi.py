9e2dcea16818f3e17cf9d95b45a7232121d3d919
juuso <juuso.kaukko@gmail.com>
2025-11-02 02:08:58 +0200

    valmis

---
+++ Module_12/Chuck_Norris_vitsi.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+
+
+pyyntö = "https://api.chucknorris.io/jokes/random"
+'''vastaus = requests.get(pyyntö)
+json_vastaus = vastaus.json()
+print(json.dumps(json_vastaus, indent=2))'''
+
+
+
+try:
+    vastaus = requests.get(pyyntö)
+    if vastaus.status_code == 200:
+        json_vastaus = vastaus.json()
+        print(json_vastaus["value"])
+except requests.exceptions.RequestException as e:
+    print("Hakua ei voitu suorittaa.")
