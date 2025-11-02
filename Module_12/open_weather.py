931f1cbc8a181bc6d7ce05d2eb7a4a84136d54d7
juuso <juuso.kaukko@gmail.com>
2025-11-02 02:48:54 +0200

    valmis

--- Module_12/open_weather.py
+++ Module_12/open_weather.py
@@ -5,15 +5,14 @@ avain = "7ec1a8950498d1ef3998510266e785d4"
 
 search = input("Enter municipality name: ")
 
-request = f"https://api.openweathermap.org/data/2.5/weather?q={search}&appid={avain}"
-vastaus = requests.get(request)
-json_vastaus = vastaus.json()
-print(json.dumps(json_vastaus, indent=2))
+request = f"https://api.openweathermap.org/data/2.5/weather?q={search}&appid={avain}&units=metric&lang=fi"
 
-'''try:
-    vastaus = requests.get(search)
+try:
+    vastaus = requests.get(request)
     if vastaus.status_code == 200:
         json_vastaus = vastaus.json()
-        print(json_vastaus[])
+        sää_tuloste = json_vastaus["weather"][0]["description"]
+        lämpötila = json_vastaus["main"]["temp"]
+        print(f"Weather: {sää_tuloste}, Temperature: {lämpötila} Celsius")
 except requests.exceptions.RequestException as e:
-    print("Hakua ei voitu suorittaa.")'''
+    print("Hakua ei voitu suorittaa.")
