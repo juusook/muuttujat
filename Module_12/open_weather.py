d501df88a0a742870a164a55b174abe476f6d2fe
juuso <juuso.kaukko@gmail.com>
2025-11-02 02:31:26 +0200

    melkein valmis.

--- Module_12/open_weather.py
+++ Module_12/open_weather.py
@@ -1,12 +1,19 @@
 import json
 import requests
 
+avain = "7ec1a8950498d1ef3998510266e785d4"
+
 search = input("Enter municipality name: ")
 
-try:
-    vastaus = requests.get(pyyntö)
+request = f"https://api.openweathermap.org/data/2.5/weather?q={search}&appid={avain}"
+vastaus = requests.get(request)
+json_vastaus = vastaus.json()
+print(json.dumps(json_vastaus, indent=2))
+
+'''try:
+    vastaus = requests.get(search)
     if vastaus.status_code == 200:
         json_vastaus = vastaus.json()
-        print(json_vastaus["value"])
+        print(json_vastaus[])
 except requests.exceptions.RequestException as e:
-    print("Hakua ei voitu suorittaa.")
+    print("Hakua ei voitu suorittaa.")'''
